d87d8bc2f9c7f1cc433f539da3ac9e812ff8b1bc
Helolara <heloise.lara@escola.pr.gov.br>
2024-07-01 22:41:49 -0300

    prova trimestral

---
+++ programação1.js
@@ -0,0 +1,9 @@
+const meses =["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+
+for(let i=0; i < meses.length; i++){
+    if (meses [i] === 'Dezembro') {
+        console.log ("Nasci em Dezembro");
+    } else{
+        console.log (meses [i]);
+    } 
+} 
